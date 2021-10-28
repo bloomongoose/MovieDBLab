e8c2123a817e31ab0846f7747af48063a6b113f8
radeen <radeen013@gmail.com>
2021-10-28 09:02:52 -0400

    laboratory

---
+++ MovieDBLab/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace MovieDBLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LET'S GOOOOOOOOOOO");
+
+            //CreateDB();
+            
+        }
+        
+        //method to create DB
+        static void CreateDB()
+        {
+            using (MovieDBContext context = new MovieDBContext())
+            {
+                Movie m1 = new Movie
+                {
+                    Title = "Parasite",
+                    Genre = "Drama",
+                    Runtime = 2.25
+                };
+                Movie m2 = new Movie
+                {
+                    Title = "Eighth Grade",
+                    Genre = "Drama",
+                    Runtime = 1.50
+                };
+                Movie m3 = new Movie
+                {
+                    Title = "Ford v Ferrari",
+                    Genre = "Drama",
+                    Runtime = 2.50
+                };
+                Movie m4 = new Movie
+                {
+                    Title = "Inglourioous Basterds",
+                    Genre = "Drama",
+                    Runtime = 2.50
+                };
+                Movie m5 = new Movie
+                {
+                    Title = "Moneyball",
+                    Genre = "Drama",
+                    Runtime = 2.25
+                };
+                Movie m6 = new Movie
+                {
+                    Title = "Borat",
+                    Genre = "Comedy",
+                    Runtime = 1.50
+                };
+                Movie m7 = new Movie
+                {
+                    Title = "Knives Out",
+                    Genre = "Comedy",
+                    Runtime = 2.25
+                };
+                Movie m8 = new Movie
+                {
+                    Title = "The Hangover",
+                    Genre = "Comedy",
+                    Runtime = 1.50
+                };
+                Movie m9 = new Movie
+                {
+                    Title = "The Other Guys",
+                    Genre = "Comedy",
+                    Runtime = 2.00
+                };
+                Movie m10 = new Movie
+                {
+                    Title = "Friday",
+                    Genre = "Comedy",
+                    Runtime = 1.50
+                };
+                Movie m11 = new Movie
+                {
+                    Title = "Howl's Moving Castle",
+                    Genre = "Animated",
+                    Runtime = 2.00
+                };
+                Movie m12 = new Movie
+                {
+                    Title = "Spider-Man: Into the Spider-Verse",
+                    Genre = "Animated",
+                    Runtime = 2.00
+                };
+                Movie m13 = new Movie
+                {
+                    Title = "Demon Slayer: Mugen Train",
+                    Genre = "Animated",
+                    Runtime = 2.00
+                };
+                Movie m14 = new Movie
+                {
+                    Title = "Klaus",
+                    Genre = "Animated",
+                    Runtime = 1.50
+                };
+                Movie m15 = new Movie
+                {
+                    Title = "Your Name",
+                    Genre = "Animated",
+                    Runtime = 1.75
+                };
+                Movie m16 = new Movie
+                {
+                    Title = "How to Train Your Dragon",
+                    Genre = "Animated",
+                    Runtime = 1.75
+                };
+                Movie m17 = new Movie
+                {
+                    Title = "District 9",
+                    Genre = "SciFi",
+                    Runtime = 2.00
+                };
+                Movie m18 = new Movie
+                {
+                    Title = "Arrival",
+                    Genre = "SciFi",
+                    Runtime = 2.00
+                };
+                Movie m19 = new Movie
+                {
+                    Title = "Dune",
+                    Genre = "SciFi",
+                    Runtime = 2.50
+                };
+                Movie m20 = new Movie
+                {
+                    Title = "The Martian",
+                    Genre = "SciFi",
+                    Runtime = 2.50
+                };
+                Movie m21 = new Movie
+                {
+                    Title = "Blade Runner",
+                    Genre = "SciFi",
+                    Runtime = 2.00
+                };
+
+                context.Movies.Add(m1);
+                context.Movies.Add(m2);
+                context.Movies.Add(m3);
+                context.Movies.Add(m4);
+                context.Movies.Add(m5);
+                context.Movies.Add(m6);
+                context.Movies.Add(m7);
+                context.Movies.Add(m8);
+                context.Movies.Add(m9);
+                context.Movies.Add(m10);
+                context.Movies.Add(m11);
+                context.Movies.Add(m12);
+                context.Movies.Add(m13);
+                context.Movies.Add(m14);
+                context.Movies.Add(m15);
+                context.Movies.Add(m16);
+                context.Movies.Add(m17);
+                context.Movies.Add(m18);
+                context.Movies.Add(m19);
+                context.Movies.Add(m20);
+                context.Movies.Add(m21);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
